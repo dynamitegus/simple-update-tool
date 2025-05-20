b6409b58fff2e8db174f34da55a2defe76e27ea2
dynamitegus <dynamitegus@dynamitegus.org>
2025-05-20 16:03:34 +1000

    what ever the hell i just did

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 use std::env;
 
+fn check_my_split_args(args: Vec<&str>, arged_arg: &str) -> bool {
+    for arg in args {
+        if arged_arg == arg {
+            return true;
+        }
+    }
+    false // Move this outside the loop to check all arguments
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let tack_args = &args[1]; // figure out how remove or ignore the tack
-    println!("{}", &args[1]);
-    if args[1] == "-h" {
+    let passed_args: Vec<String> = env::args().collect();
+    let split_args = &passed_args[1].split(""); // figure out how remove or ignore the tack
+    let my_args_have_been_splited = split_args.clone().collect::<Vec<&str>>();
+    println!("{}", &passed_args[1]);
+    println!("{}", my_args_have_been_splited[1]);
+    if check_my_split_args(my_args_have_been_splited, "h") {
         println!("HELP")
     };
-
 }
