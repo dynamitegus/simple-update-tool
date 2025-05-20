6ada59960955cb84d919686ecfef321d80be7b26
dynamitegus <dynamitegus@dynamitegus.org>
2025-05-20 16:17:06 +1000

    check for one or two tack

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,19 @@ fn check_my_split_args(args: Vec<&str>, arged_arg: &str) -> bool {
 
 fn main() {
     let passed_args: Vec<String> = env::args().collect();
-    let split_args = &passed_args[1].split(""); // figure out how remove or ignore the tack
+    let split_args = &passed_args[1].split("");
     let my_args_have_been_splited = split_args.clone().collect::<Vec<&str>>();
     println!("{}", &passed_args[1]);
-    println!("{}", my_args_have_been_splited[1]);
+
+    if (my_args_have_been_splited[1] == "-") && (my_args_have_been_splited[2] == "-") {
+        println!("two tack");
+    } else if (my_args_have_been_splited[1] == "-") && (my_args_have_been_splited[2] != "-") {
+        println!("one tack");
+    } else {
+        println!("UH OH NO TACK");
+    }
+
     if check_my_split_args(my_args_have_been_splited, "h") {
-        println!("HELP")
+        println!("HELP");
     };
 }
