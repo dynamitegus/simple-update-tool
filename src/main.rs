daab5d586a4b97e8fe9923e19c6a3bb9d4679ebd
dynamitegus <dynamitegus@dynamitegus.org>
2025-05-20 12:59:32 +1000

    comment

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let tack_args = &args[1];
+    let tack_args = &args[1]; // figure out how remove or ignore the tack
     println!("{}", &args[1]);
     if args[1] == "-h" {
         println!("HELP")
