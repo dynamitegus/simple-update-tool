e78db355aebeea03019a7ffc7b42ece6502849be
dynamitegus <dynamitegus@dynamitegus.org>
2025-05-20 12:49:22 +1000

    I figured out how it works

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
-    println!("Hello, Github!");
+    let tack_args = &args[1];
+    println!("{}", &args[1]);
+    if args[1] == "-h" {
+        println!("HELP")
+    };
+
 }
